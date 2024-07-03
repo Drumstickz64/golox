20030a650bbd2ec6a43bc3977eefbcb5f29c58e6
Drumstickz64 <gearman533@gmail.com>
2024-07-04 02:04:28 +0300

    add rpn printing

--- ast/print.go
+++ ast/print.go
@@ -41,3 +41,41 @@ func (p Printer) parenthesize(name string, exps ...Expr) string {
 
 	return result
 }
+
+type RPNPrinter struct{}
+
+func (p RPNPrinter) Print(exp Expr) string {
+	return exp.Accept(p).(string)
+}
+
+func (p RPNPrinter) VisitBinary(exp *Binary) any {
+	return p.rpnesize(exp.Operator.Lexeme, exp.Left, exp.Right)
+}
+
+func (p RPNPrinter) VisitGrouping(exp *Grouping) any {
+	return exp.Expression.Accept(p)
+}
+
+func (p RPNPrinter) VisitLiteral(exp *Literal) any {
+	if exp.Value == nil {
+		return "nil"
+	}
+
+	return fmt.Sprint(exp.Value)
+}
+
+func (p RPNPrinter) VisitUnary(exp *Unary) any {
+	return p.rpnesize(exp.Operator.Lexeme, exp.Right)
+}
+
+func (p RPNPrinter) rpnesize(name string, exps ...Expr) string {
+	result := ""
+
+	for _, exp := range exps {
+		result += exp.Accept(p).(string) + " "
+	}
+
+	result += name
+
+	return result
+}
--- main.go
+++ main.go
@@ -8,18 +8,49 @@ import (
 	"os"
 	"strings"
 
+	"github.com/Drumstickz64/golox/ast"
 	"github.com/Drumstickz64/golox/reporting"
 	"github.com/Drumstickz64/golox/scanning"
+	"github.com/Drumstickz64/golox/token"
 )
 
 func main() {
-	if len(os.Args) == 1 {
-		RunPrompt()
-	} else if len(os.Args) == 2 {
-		RunFile(os.Args[1])
-	} else {
-		reporting.CliError("Usage: golox [script]", 64)
+	expression := ast.Binary{
+		Left: &ast.Grouping{
+			Expression: &ast.Binary{
+				Left: &ast.Literal{Value: 1},
+				Operator: token.Token{
+					Kind:   token.PLUS,
+					Lexeme: "+",
+				},
+				Right: &ast.Literal{Value: 2},
+			},
+		},
+		Operator: token.Token{
+			Kind:   token.STAR,
+			Lexeme: "*",
+		},
+		Right: &ast.Grouping{
+			Expression: &ast.Binary{
+				Left: &ast.Literal{Value: 4},
+				Operator: token.Token{
+					Kind:   token.MINUS,
+					Lexeme: "-",
+				},
+				Right: &ast.Literal{Value: 3},
+			},
+		},
 	}
+
+	fmt.Println(ast.RPNPrinter{}.Print(&expression))
+
+	// if len(os.Args) == 1 {
+	// 	RunPrompt()
+	// } else if len(os.Args) == 2 {
+	// 	RunFile(os.Args[1])
+	// } else {
+	// 	reporting.CliError("Usage: golox [script]", 64)
+	// }
 }
 
 func RunFile(path string) {
